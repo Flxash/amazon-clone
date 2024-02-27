420897ec3536aa01d2f4d1b45c42fcc8f2ae7cc1
Abdel Yasser <zahabys@gmail.com>
2024-02-26 22:47:06 -0700

    Cart quantity updates on page load + default cart is empty

--- data/products.js
+++ data/products.js
@@ -1,13 +1,13 @@
 export function getProduct(productId) {
-  let matchingProduct;
+let matchingProduct;
 
-  products.forEach((product) => {
-      if (product.id === productId) {
-          matchingProduct = product;
-      }
-  });
-  
-  return matchingProduct;
+products.forEach((product) => {
+    if (product.id === productId) {
+        matchingProduct = product;
+    }
+});
+
+return matchingProduct;
 }
 
 export const products = [
